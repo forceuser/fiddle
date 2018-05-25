46d286a31517ad4f87f05ab03cfca162a1710d8c
Vitaly Dub <forceuserz@gmail.com>
2018-05-25 20:06:03 +0300

    Update pz.js

--- pz.js
+++ pz.js
@@ -4,5 +4,5 @@ function toDom (html) {
  	return [...d.childNodes];
 }
 
-const container = toDom(`<div><img src=""></div>`)[0];
+const container = toDom(`<div><img src="./red-panda-1182078_960_720.jpg"></div>`)[0];
 document.body.appendChild(container);
